d6071a01d963caa7acac604882be1f11d2d9d47a
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2023-06-07 23:41:18 -0300

    feat/fix: search by name or number

--- mask.js
+++ mask.js
@@ -1,13 +1,10 @@
-IMask(document.querySelector("#dddContact"), {
-    mask: IMask.MaskedRange,
-    from: 11,
-    to: 99,
-});
-
 IMask(document.querySelector("#numContact"), {
-    mask: "00000{-}0000",
-});
-
-IMask(document.querySelector("#nomeContact"), {
-    mask: /^[a-zA-Z\s]+$/,
+    mask: "{(}DDD{)} 00000{-}0000",
+    blocks: {
+        DDD: {
+            mask: IMask.MaskedRange,
+            from: 11,
+            to: 99,
+        },
+    },
 });
